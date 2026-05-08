ada05db5ace73afff4930eb3e6ae3d03877f78ba
ArpitU <87164723+ArpitU@users.noreply.github.com>
2026-05-08 22:07:39 +0530

    Initial commit

---
+++ i.java
@@ -0,0 +1,22 @@
+// public class i{
+//   public static void main(String[] args) {
+//     System.out.println("Hello World");
+//   }
+// }
+
+// public class i{
+//   public static void main(String[] args) {
+//     int a = 10;
+//     int b = 20;
+//     int c = a + b;
+//     System.out.println("The sum of a and b is: " + c);
+//   }
+// }
+// public class i{
+//   public static void main(String[] args) {
+//     float a = 10;
+//     float b = 20;
+
+//     System.out.println("The sum of a and b is: " + (a/b));
+//   }
+// }
