265b5e72bbfcf743ded270ff9cfcab255eb7b81f
ArpitU <87164723+ArpitU@users.noreply.github.com>
2026-05-10 14:03:45 +0530

    Initial commit

---
+++ one.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class one
+{
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+String firstName = scanner.nextLine();
+String lastName = scanner.nextLine();
+String fullName = firstName +' '+ lastName;
+System.out.println(fullName);
+    }
+}   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ strings.java
@@ -0,0 +1,8 @@
+public class strings {
+    public static void main(String[] args) {
+       String  s ="I love java";
+       System.out.println(s);
+          System.out.println(s.charAt(0));
+            System.out.println(s.charAt(2));
+    }
+}
